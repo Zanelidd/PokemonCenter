11de09c13ffcb0cf1e6dd21471eec77dea4dd79d
Damien Arnaud <damien.arnaudpro@gmail.com>
2024-10-22 17:26:07 +0200

    Add communication with backend

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,9 +2,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import style from "./card.module.css";
 import { useCollection } from "../../services/stores/CollectionStore";
 import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import loadingGif from "/ピカチュウ-pokeball.gif";
 import { useState } from "react";
+import type { Card } from "pokemon-tcg-sdk-typescript/dist/sdk";
 
 const Card = () => {
   const navigate = useNavigate();
@@ -17,6 +18,34 @@ const Card = () => {
 
   const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 
+  const mutation = useMutation({
+    mutationFn: async (cardId: string) => {
+      return await fetch(`${import.meta.env.VITE_BACKEND_URL}/collection`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ cardId }),
+      })
+        // .then((res) => {
+        //   res.json();
+        // })
+        .then((res) => {
+          res.json();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    // onSuccess: (data) => {
+    //   console.log("Success:", data);
+    // },
+    // onError: (error) => {
+    //   console.error("Error:", error);
+    // },
+  });
   const { isPending, error, data } = useQuery({
     queryKey: ["PokemonCard", `${params.cardId}`],
     queryFn: () => {
@@ -26,14 +55,17 @@ const Card = () => {
   });
 
   if (isPending) {
-    return (
-      <img className={style.loadingGif} src={loadingGif} alt="Loading Gif" />
-    );
+    return <img className="loadingGif" src={loadingGif} alt="Loading Gif" />;
   }
 
   if (error) {
     return "An error occured: " + error.message;
   }
+
+  const handleAddCollection = (data: Card) => {
+    addToCollection(data);
+    mutation.mutate(data.id);
+  };
 
   return (
     <div className={style.cardInfoContainer}>
@@ -72,12 +104,51 @@ const Card = () => {
           <img className={style.cardImg} src={data.images.large} alt="" />
         </div>
       </div>
-      <div className={style.infosContainer}>
+      <section className={style.infosContainer}>
+        <div>
+          <h2>{data.name}</h2>
+          <h3>{data.set.name}</h3>
+          <h3>{data.types}</h3>
+          <h3>{data.supertype}</h3>
+
+          <h3>Evolves from {data.evolvesFrom}</h3>
+        </div>
         <div className={style.textInfo}>
+          <div className={style.attackContainer}>
+            {data.attacks?.map((attack) => {
+              return (
+                <div key={attack.text} className={style.attack}>
+                  <div className={style.attackTitle}>
+                    <p>{attack.name}</p>
+                    <p>{attack.damage}</p>
+                  </div>
+                  <p>{attack.text}</p>
+                </div>
+              );
+            })}
+          </div>
           <p>{data.flavorText}</p>
-          <p>Rarity : {data.rarity}</p>
-          <p>Card number : {data.number}</p>
-          {/* <p>Average Price : {data.cardmarket.prices.trendPrice} $ </p> */}
+          <div className={style.priceINfo}>
+            <p>Prices</p>
+            <div className={style.normalPrice}>
+              <p>Low Price : {data.tcgplayer?.prices.normal?.low} $ </p>
+              <p>Average Price : {data.tcgplayer?.prices.normal?.market} $</p>
+              <p>High Price : {data.tcgplayer?.prices.normal?.high} $ </p>
+            </div>
+            <div className={style.hollowPrice}>
+              <p>Hollow Low Price : {data.tcgplayer?.prices.holofoil?.low} $</p>
+              <p>
+                Hollow Average Price : {data.tcgplayer?.prices.holofoil?.market}
+                $
+              </p>
+              <p>
+                Hollow High Price : {data.tcgplayer?.prices.holofoil?.high} $
+              </p>
+            </div>
+            <p>
+              Card number : {data.number}/{data.set.total}
+            </p>
+          </div>
         </div>
         <div className={style.buttonContainer}>
           {isInCollection ? (
@@ -91,7 +162,7 @@ const Card = () => {
           ) : (
             <button
               onClick={() => {
-                addToCollection(data);
+                handleAddCollection(data);
               }}
             >
               Add to my collection
@@ -105,7 +176,7 @@ const Card = () => {
             Back
           </button>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
