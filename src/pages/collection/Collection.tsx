770486c746df3d5e580050215969c0c8ebdef59d
Damien Arnaud <damien.arnaudpro@gmail.com>
2025-03-20 11:32:52 +0100

    add a filter with select

--- src/pages/collection/Collection.tsx
+++ src/pages/collection/Collection.tsx
@@ -9,75 +9,100 @@ import { useUser } from '../../stores/UserStore.tsx';
 import { useMutation } from '@tanstack/react-query';
 import CardSkeleton from '../../components/skeletons/card-skeleton/CardSkeleton.tsx';
 import { showError } from '../../utils/toastUtils.ts';
+import SelectFilter from '../../components/selectFilter/SelectFilter.tsx';
+import { Card } from 'pokemon-tcg-sdk-typescript/dist/interfaces/card';
+import { Type } from 'pokemon-tcg-sdk-typescript/dist/enums/type';
 
 const Collection = () => {
-  const { collection } = useCollection();
-  const { user } = useUser();
+    const { collection } = useCollection();
+    const { user } = useUser();
+    const [filterTags, setFilterTags] = useState<Record<string, string>>({});
 
-  const [pagination, setPagination] = useState({
-    pageIndex: 0,
-    pageSize: 20,
-  });
+    const filteredData = collection.filter((card: Card) => {
+      if (filterTags) {
+        return Object.entries(filterTags).every(([category, value]) => {
+          switch (category) {
+            case 'set':
+              return Array.isArray(card.set.id) ? card.set.id.includes(value) : card.set.id === value;
+            case 'types':
+              return card.types?.includes(value as Type);
+            case 'rarity':
+              return card.rarity === value;
+            default:
+              return false;
+          }
+        });
+      }
+    });
+    const [pagination, setPagination] = useState({
+      pageIndex: 0,
+      pageSize: 20,
+    });
 
-  const { mutate: fetchCards, isPending } = useMutation({
-    mutationFn: async () => {
-      if (!user?.userId) throw new Error("User not logged in");
-      return api.card.getCard(user.userId);
-    },
-    onError: (error) => {
-      if (error instanceof Error) {
-        showError(error.message);
-      }}
+    const { mutate: fetchCards, isPending } = useMutation({
+      mutationFn: async () => {
+        if (!user?.userId) throw new Error('User not logged in');
+        return api.card.getCard(user.userId);
+      },
+      onError: (error) => {
+        if (error instanceof Error) {
+          showError(error.message);
+        }
+      },
+    });
 
+    const table = useReactTable({
+      columns: [],
+      data: collection || [],
+      getCoreRowModel: getCoreRowModel(),
+      getPaginationRowModel: getPaginationRowModel(),
+      onPaginationChange: setPagination,
+      state: {
+        pagination,
+      },
+    });
 
-  });
+    useEffect(() => {
+      if (user?.userId && collection.length === 0) {
+        fetchCards();
+      }
+    }, [user, collection.length, fetchCards]);
 
-  const table = useReactTable({
-    columns: [],
-    data: collection || [],
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onPaginationChange: setPagination,
-    state: {
-      pagination,
-    },
-  });
-
-  useEffect(() => {
-    if (user?.userId && collection.length === 0) {
-      fetchCards();
-    }
-  }, [user, collection.length, fetchCards]);
-
-  return (
-    <>
-      {isPending && <CardSkeleton />}
-
-      <div className={style.cardContainer}>
-        {collection &&
-          collection
-            .slice(
-              pagination.pageIndex * pagination.pageSize,
-              pagination.pageSize + pagination.pageSize * pagination.pageIndex
-            )
-            .map((card) => {
-              return (
-                <SearchResults key={`${card.name}${card.set}`} data={card} />
-              );
-            })}
-      </div>
-
-      {collection.length === 0 && !isPending && (
-        <div className={style.emptyCollection}>
-          Your collection is empty. Start adding cards!
+    return (
+      <>
+        {isPending && <CardSkeleton />}
+        <div className={style.filterContainer}>
+          <SelectFilter cards={collection} id={'set'} setFilterTags={setFilterTags} />
+          <SelectFilter cards={collection} id={'types'} setFilterTags={setFilterTags} />
+          <SelectFilter cards={collection} id={'rarity'} setFilterTags={setFilterTags} />
+          <button onClick={() => setFilterTags({})}>Reset</button>
         </div>
-      )}
 
-      {collection.length > 0 && (
-        <Pagination table={table} pagination={pagination} />
-      )}
-    </>
-  );
-};
+        <div className={style.cardContainer}>
+          {filteredData &&
+            filteredData
+              .slice(
+                pagination.pageIndex * pagination.pageSize,
+                pagination.pageSize + pagination.pageSize * pagination.pageIndex,
+              )
+              .map((card) => {
+                return (
+                  <SearchResults key={`${card.name}${card.set}`} data={card} />
+                );
+              })}
+        </div>
+
+        {filteredData.length === 0 && !isPending && (
+          <div className={style.emptyCollection}>
+            Your collection is empty. Start adding cards!
+          </div>
+        )}
+        {filteredData.length > 0 && (
+          <Pagination table={table} pagination={pagination} />
+        )}
+      </>
+    );
+  }
+;
 
 export default Collection;
--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -2,3 +2,4 @@ import { Card } from 'pokemon-tcg-sdk-typescript/dist/sdk';
 
 export type CollectionCard = Card & { collectionId?: number };
 export type cardResponse = { id: number, remoteId: string, userId: number };
+
