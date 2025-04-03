94ec25e1f39a38fe8d21bb982a86f9f5a4c61f0e
Damien Arnaud <damien.arnaudpro@gmail.com>
2025-04-03 15:51:52 +0200

    change in css

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -3,4 +3,3 @@ import {Card} from 'pokemon-tcg-sdk-typescript/dist/sdk';
 export type CollectionCard = Card & { collectionId?: number };
 export type cardResponse = { id: number, remoteId: string, userId: number };
 
-s
