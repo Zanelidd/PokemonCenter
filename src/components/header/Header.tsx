09ad5c99742f05bec64937f83cd7dc045fdbe65f
Damien Arnaud <damien.arnaudpro@gmail.com>
2026-04-16 14:11:33 +0200

    fix : clean console log

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,13 @@
 import {NavLink} from 'react-router-dom';
 import style from './header.module.css';
-import {useUser} from '../../stores/UserStore';
+import {useAuthActions, useUser} from '../../stores/UserStore';
 
 import {useState} from "react";
 import SearchCard from "../searchCard/SearchCard.tsx";
 
 const Header = () => {
-    const {toggleModal, showModal, isAuthenticated, logout} = useUser();
+    const {toggleModal, setLogout} = useAuthActions();
+    const {isAuthenticated, isModalOpen} = useUser();
     const [openBurger, setOpenBurger] = useState(false);
 
 
@@ -17,49 +18,47 @@ const Header = () => {
     }
 
 
-    return (<>
-            <header className={`${style.header} `}>
-                <div className={style.hamburgerContainer} onClick={() => setOpenBurger(!openBurger)}>
-                    <div className={style.hamburger}>
-                        <div className={`${style.burger} ${style.burger1} ${openBurger ? style.isOpen : ""}`}></div>
-                        <div className={`${style.burger} ${style.burger2} ${openBurger ? style.isOpen : ""}`}></div>
-                        <div className={`${style.burger} ${style.burger3} ${openBurger ? style.isOpen : ""}`}></div>
-                    </div>
+    return <>
+        <header className={`${style.header} `}>
+            <div className={style.hamburgerContainer} onClick={() => setOpenBurger(!openBurger)}>
+                <div className={style.hamburger}>
+                    <div className={`${style.burger} ${style.burger1} ${openBurger ? style.isOpen : ""}`}></div>
+                    <div className={`${style.burger} ${style.burger2} ${openBurger ? style.isOpen : ""}`}></div>
+                    <div className={`${style.burger} ${style.burger3} ${openBurger ? style.isOpen : ""}`}></div>
                 </div>
-                <div className={`${openBurger ? style.headerContainerBurger : style.headerContainer}`}>
-                    <nav className={`${style.linkContainer} ${openBurger ? style.navOpen : ''}`}>
-                        <NavLink to="/home" onClick={() => setOpenBurger(false)}>Home</NavLink>
-                        {isAuthenticated &&
-                            <NavLink to="user/collection" onClick={() => setOpenBurger(false)}>Collection</NavLink>}
-                    </nav>
-                    {!openBurger && <SearchCard/>}
-                    <nav className={`${style.loginContainer} ${openBurger ? style.navOpen : ''}`}>
-                        {isAuthenticated &&
-                            <NavLink to="user/account" onClick={() => setOpenBurger(false)}>Account</NavLink>}
-                        {isAuthenticated ? (
-                            <button
+            </div>
+            <div className={`${openBurger ? style.headerContainerBurger : style.headerContainer}`}>
+                <nav className={`${style.linkContainer} ${openBurger ? style.navOpen : ''}`}>
+                    <NavLink to="/home" onClick={() => setOpenBurger(false)}>Home</NavLink>
+                    {isAuthenticated &&
+                        <NavLink to="user/collection" onClick={() => setOpenBurger(false)}>Collection</NavLink>}
+                </nav>
+                {!openBurger && <SearchCard/>}
+                <nav className={`${style.loginContainer} ${openBurger ? style.navOpen : ''}`}>
+                    {isAuthenticated &&
+                        <NavLink to="user/account" onClick={() => setOpenBurger(false)}>Account</NavLink>}
+                    {isAuthenticated ? (
+                        <button
+                            onClick={() => {
+                                setLogout();
+                                setOpenBurger(false)
+                            }}
+                        >
+                            Logout
+                        </button>
+                    ) : (
+                        <button type="button" disabled={isModalOpen}
                                 onClick={() => {
-                                    logout();
-                                    setOpenBurger(false)
+                                    handleModalOpen();
                                 }}
-                            >
-                                Logout
-                            </button>
-                        ) : (
-                            <button type="button" disabled={showModal}
-                                    onClick={() => {
-                                        handleModalOpen();
-                                    }}
-                            >
-                                Connect
-                            </button>
-                        )}
-                    </nav>
-                </div>
-            </header>
-        </>
-    )
-        ;
+                        >
+                            Connect
+                        </button>
+                    )}
+                </nav>
+            </div>
+        </header>
+    </>;
 };
 
 export default Header;
